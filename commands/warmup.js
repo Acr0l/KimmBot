b64e1d1172693342bc11307d9da4122a664ccdd0
scar05 <oscar.munoz.dlf@gmail.com>
2021-09-17 01:02:00 -0300

    Corrección de respuestas en warmup

--- commands/warmup.js
+++ commands/warmup.js
@@ -3,12 +3,12 @@ const { MessageActionRow, MessageButton, MessageSelectMenu  } = require('discord
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 
-const languageQuestions = ["¿Cuál es la tercera letra de 'Chocapic'?"];
-const languageAnswers =   ["o"]
-const languajeIncorrect1 = ["p"];
-const languajeIncorrect2 = ["a"];
-const languajeIncorrect3 = ["i"];
-const languajeIncorrect4 = ["c"];
+const languageQuestions = ["¿Cuál es la tercera letra de 'Chocapic'?","¿Cuántos idiomas existen?"];
+const languageAnswers =   ["o","6.909"]
+const languajeIncorrect1 = ["p","1.503"];
+const languajeIncorrect2 = ["a","642"];
+const languajeIncorrect3 = ["i","9.012"];
+const languajeIncorrect4 = ["c","320"];
 
 const historyQuestions = ["¿Qué es Holanda?","¿Cuál es la capital de los Países Bajos?","¿Qué le pasa a lupita?"];
 const historyAnswers   = ["Una región histórica","Ámsterdam","No sé"];
@@ -29,38 +29,44 @@ module.exports = {
                 .addChoice('Math', 1)
                 .addChoice('Language', 2)
                 .addChoice('History and Geography', 3)
-                .addChoice('Sciences', 4)
-                .addChoice('Computer Sciences', 5)
-                .addChoice('Life Skills', 6)
-                .addChoice('Musical Theory (highly recommended)', 7)
+                //.addChoice('Sciences', 4)
+                //.addChoice('Computer Sciences', 5)
+                //.addChoice('Life Skills', 6)
+                //.addChoice('Musical Theory (highly recommended)', 7)
                 ),
 
-    async execute(interaction) {
-
-        
-        if (interaction.isSelectMenu())
-        {
-            if (interaction.customId === 'select') {
-             await interaction.reply("¡Gracias por tu respuesta!");
-            }
-        }
-        else        
-        {    
+    async execute(interaction) {     
+            
+            
             const subject = interaction.options.getInteger('subject');        
             
             let currQuestion = "Aquí va una pregunta";
             let alternativesOrdered = ['Correcto','Casi correcto','Incorrecto','Pal pico','Todas las anteriores'];
 
-            if      (subject == 1){currQuestion = "Aquí va una pregunta de matemática"}
+            if      (subject == 1){
+                const numbers = [];
+                for (let i = 0 ; i < 5 ; i++)
+                {
+                    numbers[i] = Math.trunc(Math.random() * 21);
+                }
+
+                currQuestion = "¿Cuánto es " + numbers[0] + " + " + numbers[1] + " - " + numbers[2] + " x " + numbers[3] + "?";
+                alternativesOrdered[0] = (numbers[0] + numbers[1] - (numbers[2] * numbers[3])).toString();
+                alternativesOrdered[1] = ((numbers[0] + numbers[1] - numbers[2]) * numbers[3]).toString();
+                alternativesOrdered[2] = (numbers[0] + numbers[1] + numbers[2] * numbers[3]).toString();
+                alternativesOrdered[3] = (numbers[0] + numbers[2] - numbers[1] * numbers[3]).toString();
+                alternativesOrdered[4] = (numbers[0] - numbers[1] - numbers[2] * numbers[3]).toString();
+
+            }
 
             else if (subject == 2){
-                const indexQuestion = Math.trunc(Math.random() * (languageQuestions.length + 1));
+                const indexQuestion = Math.trunc(Math.random() * languageQuestions.length);
                 currQuestion = languageQuestions[indexQuestion];
                 alternativesOrdered = [languageAnswers[indexQuestion],languajeIncorrect1[indexQuestion],languajeIncorrect2[indexQuestion],languajeIncorrect3[indexQuestion],languajeIncorrect4[indexQuestion]]
             }
 
             else if (subject == 3){
-                const indexQuestion = Math.trunc(Math.random() * (historyQuestions.length + 1));
+                const indexQuestion = Math.trunc(Math.random() * historyQuestions.length);
                 currQuestion = historyQuestions[indexQuestion];
                 alternativesOrdered = [historyAnswers[indexQuestion],historyIncorrect1[indexQuestion],historyIncorrect2[indexQuestion],historyIncorrect3[indexQuestion],historyIncorrect4[indexQuestion]];            
             }
@@ -69,64 +75,65 @@ module.exports = {
                 currQuestion = "Pta justo estoy trabajando en este ramo, prueba otro crack :D";
             }
 
-            
-            let alternatives = ["A","B","C","D","E"];
-            /*for (let i = 0; i < 5 ; i++)
+            //Random order
+            let alternatives = ["","","","",""];
+            let answers = ['','','','',''];
+            const abecedary = ['A','B','C','D','E'];
+            for (let i = 0; i < 5 ; i++)
             {
-                const currAlternative = alternativesOrdered[Math.trunc(Math.random() * 6)];
-                let exists = false;
-                for  (let x = 0; x < alternatives.length; x++)
+                let filled = true;
+                let index;
+                while(filled)
                 {
-                    if(currAlternative == alternatives[x]) exists=true;
+                    index = Math.trunc(Math.random()*5);
+                    if(alternatives[index] == "") filled = false;
                 }
-                if (!exists) alternatives[i] = currAlternative;
-            }*/
-
-            alternatives = alternativesOrdered;
-
+                alternatives[index] = alternativesOrdered[i];
+                answers[index] = abecedary[i];
+            }
 
             const embed = new MessageEmbed()
-            .setColor('#57CC98')
+            .setColor('#39A2A5')
             .setAuthor('Warm up...')
             //.setAuthor('Kimm Bot', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
             .setTitle(currQuestion)
-            .setFooter('No funciona la correción asiq tranqi');
+            .setFooter('Cuidado con tu energía mental');
 
 
             const row = new MessageActionRow()
             .addComponents(
                 new MessageSelectMenu()
-                    .setCustomId('select')
-                    .setPlaceholder('Select an option')
+                    .setCustomId('warmupSelect')
+                    .setPlaceholder('Selecciona una alternativa')
                     .setMinValues(1)
                     .setMaxValues(1)
                     .addOptions([
                         {
                             label: alternatives[0],
-                            value: "A",
+                            value: answers[0],
                         },
                         {
                             label: alternatives[1],
-                            value: "B",
+                            value: answers[1],
                         },
                         {
                             label: alternatives[2],
-                            value: "C",
+                            value: answers[2],
                         },
                         {
                             label: alternatives[3],
-                            value: "D",
+                            value: answers[3],
                         },
                         {
                             label: alternatives[4],
-                            value: "E",
+                            value: answers[4],
                         },                    
                     ]),
             );
 
             
             await interaction.reply({ embeds: [embed], ephemeral : true , components: [row] }); 
-        }       
+               
     }
 
 };
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Require the necesaary discord.js classes
 
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, Intents, MessageEmbed  } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('fs');
 
 // Create client
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -37,6 +37,45 @@ client.once('ready', () => {
 
 // Initializing the commands.
 client.on('interactionCreate', async interaction => {
+    if (interaction.isSelectMenu())
+    {
+        if (interaction.customId === 'warmupSelect')
+        {
+            const value = interaction.values[0];
+            const username = interaction.user.username; 
+
+            if(value == 'A')
+            {
+                const embed = new MessageEmbed()
+                .setColor('#80EA98')
+                //.setAuthor('Warm up...')
+                //.setAuthor('Kimm Bot', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+                .setTitle("Respuesta correcta " + username + " 😎👍")
+                .setThumbnail('https://freepikpsd.com/media/2019/10/correcto-incorrecto-png-7-Transparent-Images.png')
+                .setFooter('Ahora callate callao');
+                interaction.update({ embeds:[embed], components:[] , ephemeral : true });
+            }
+            else
+            {
+                const embed = new MessageEmbed()
+                .setColor('#eb3434')
+                //.setAuthor('Warm up...')
+                //.setAuthor('Kimm Bot', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+                .setTitle("Pta " + username + " que eri aweonao 🙄")
+                .setThumbnail('https://cdn.pixabay.com/photo/2012/04/12/20/12/x-30465_960_720.png')
+                .setFooter('Respuesta incorrecta por si acaso');
+                interaction.update({ embeds:[embed], components: [], ephemeral : true });
+            }
+            
+        }
+
+        else
+
+        {
+            interaction.reply('Callate callao');
+        }
+    }
+
     if (!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
