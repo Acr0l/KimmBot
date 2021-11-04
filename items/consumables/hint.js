cfe739e6225e8c9d900b07325e5e2a194adad46a
scar05 <oscar.munoz.dlf@gmail.com>
2021-11-04 19:01:04 -0300

    Embed Item Use Hint

--- items/consumables/hint.js
+++ items/consumables/hint.js
@@ -1,6 +1,7 @@
 const { translate } = require("../../handlers/language"),
     { getActivity } = require("../../handlers/activity"),
-    quizDatabase = require("../../models/quizSchema");
+    quizDatabase = require("../../models/quizSchema"),
+    { MessageEmbed } = require("discord.js");
 
 module.exports = {
     /**
@@ -34,12 +35,20 @@ module.exports = {
             let fewAlternatives = [quiz.correct_answer, ...quiz.incorrect_answers]
                 .slice(0, 3)
                 .sort(() => Math.random() - 0.5);
-            interaction.reply(
-                `${translate(
-                    guild,
-                    "HINT_SUCCESSFUL_REPLY"
-                )}\n${fewAlternatives.join("\n")}`
-            );
+
+            const embed = new MessageEmbed()
+                .setColor(`#39A2A5`)
+                .setTitle(translate(guild, 'HINT_SUCCESSFUL_REPLY'))
+            ;
+
+            let counter = 0;
+            for (const alternative of fewAlternatives){
+                counter++;
+                embed.addField( counter + '°', '`' + alternative + '`' , true );
+            }
+            
+            interaction.reply({embeds : [embed], ephemeral : true});
+            
         } catch (e) {
             interaction.reply(translate(guild, "HINT_NO_QUIZ"));
             console.log(e);
