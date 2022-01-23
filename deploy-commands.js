69b65f5b44cf76b60186e52d141f20286c207152
Acr0l <sb.lorca.g@gmail.com>
2022-01-22 21:50:31 -0300

    Deploy global commands & logger added

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,30 +1,49 @@
-const fs = require('fs');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
-const path = require('path');
+const fs = require('fs'),
+    { REST } = require('@discordjs/rest'),
+    { Routes } = require('discord-api-types/v9'),
+    { clientId, guildId, token } = require('./config.json'),
+    path = require('path'),
+    logger = require('./logger');
 
 const commands = [];
 
 let commandFiles = [];
 function throughDirectory(dir) {
-    fs.readdirSync(dir)
-        .forEach(file => {
-            const absolute = path.join(dir, file);
-            if (fs.statSync(absolute).isDirectory()) return throughDirectory(absolute);
-            else if (absolute.endsWith('js')) return commandFiles.push(absolute);
-        });
+    fs.readdirSync(dir).forEach((file) => {
+        const absolute = path.join(dir, file);
+        if (fs.statSync(absolute).isDirectory())
+            return throughDirectory(absolute);
+        else if (absolute.endsWith('js')) return commandFiles.push(absolute);
+    });
 }
 throughDirectory('./commands');
 for (const file of commandFiles) {
     const command = require(`./${file}`);
     if (command.data.name) commands.push(command.data.toJSON());
-    else console.log(`${file} has no name`);
-    // commands.push(command.data.toJSON());
+    else logger.info(`${file} has no name`);
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+(async () => {
+    try {
+        logger.info('Started refreshing application (/) commands.');
+
+        if (process.argv[2] === '--global') {
+            await rest.put(Routes.applicationCommands(clientId), {
+                body: commands,
+            });
+            logger.info(
+                'Successfully reloaded application global (/) commands.',
+            );
+        } else {
+            await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+                body: commands,
+            });
+
+            logger.info('Successfully reloaded application (/) commands.');
+        }
+    } catch (error) {
+        logger.error(error);
+    }
+})();
