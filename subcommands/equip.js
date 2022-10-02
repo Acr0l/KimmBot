b2cc8d7db39d65efc5919fb30ceabb8070db2a7c
Acr0l <sb.lorca.g@gmail.com>
2022-10-02 20:17:07 -0300

    Subcommands (Un)Equip v14

--- subcommands/equip.js
+++ subcommands/equip.js
@@ -1,49 +1,67 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'),
-	{ getItemList } = require('../handlers/itemInventory'),
-	{ iTranslate } = require('../handlers/language');
+const { SlashCommandBuilder } = require("@discordjs/builders"),
+  { getItemList } = require("../handlers/itemInventory"),
+  { iTranslate } = require("../handlers/language");
 
-const TRANSLATION_PATH = 'subcommands.equip';
-const REJECTION_PATH = 'rejection.items';
+const TRANSLATION_PATH = "subcommands.equip";
+const REJECTION_PATH = "rejection.items";
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('equip')
-		.setDescription('Equip selected item.'),
-	/**
-		 *
-		 * @param {import('discord.js').Interaction} interaction
-		 * @param {import('../models/profileSchema').User} userData
-		 * @returns
-		 */
-	async execute(interaction, userData) {
-		const { guild } = interaction,
-			// @ts-ignore
-			itemAction = interaction.options.getString('item'),
-			regex = /equip/;
+  data: new SlashCommandBuilder()
+    .setName("equip")
+    .setDescription("Equip selected item."),
+  /**
+   *
+   * @param {import('discord.js').Interaction} interaction
+   * @param {import('../models/profileSchema').User} userData
+   * @returns
+   */
+  async execute(interaction, userData) {
+    const { guild } = interaction,
+      // @ts-ignore
+      itemAction = interaction.options.getString("item"),
+      regex = /equip/;
 
-		try {
-			const itemList = getItemList(),
-				currentItem = itemList[Object.keys(itemList).filter((item) => itemList[item].name.toLowerCase() === itemAction.toLowerCase())],
-				// @ts-ignore
-				owned = userData.inventory.findIndex((item) => item._id === currentItem?.id);
-			if (!currentItem) {throw 'item_not_found';}
-			else if (userData.equipment.includes(currentItem?.id)) {throw 'item_equipped';}
-			else if (owned === -1) {throw 'item_not_owned';}
-			else if (currentItem && regex.test(currentItem?.use)) {
-				// Remove item from inventory
-				// @ts-ignore
-				userData.inventory.splice(owned, 1);
-				// Add item to equipment
-				userData.equipment.push(currentItem.id);
-				// @ts-ignore
-				await userData.save();
-				// @ts-ignore
-				await interaction.reply({ content: iTranslate(guild, `${TRANSLATION_PATH}.success`, { currentItem }) });
-			}
-			else {throw 'invalid_action';}
-		}
-		catch (error) {
-			// @ts-ignore
-			return await interaction.reply({ content: iTranslate(guild, `${REJECTION_PATH}.${error}`) });
-		}
-	},
+    try {
+      const itemList = getItemList(),
+        currentItem =
+          itemList[
+            Object.keys(itemList).filter(
+              (item) =>
+                itemList[item].name.toLowerCase() === itemAction.toLowerCase()
+            )
+          ],
+        // @ts-ignore
+        owned = userData.inventory.findIndex(
+          (item) => item._id === currentItem?.id
+        );
+      if (!currentItem) {
+        throw "item_not_found";
+      } else if (userData.equipment.includes(currentItem?.id)) {
+        throw "item_equipped";
+      } else if (owned === -1) {
+        throw "item_not_owned";
+      } else if (currentItem && regex.test(currentItem?.use)) {
+        // Remove item from inventory
+        // @ts-ignore
+        userData.inventory.splice(owned, 1);
+        // Add item to equipment
+        userData.equipment.push(currentItem.id);
+        // @ts-ignore
+        await userData.save();
+        // @ts-ignore
+        await interaction.reply({
+          content: iTranslate(guild, `${TRANSLATION_PATH}.success`, {
+            currentItem,
+			user: interaction.user
+          }),
+        });
+      } else {
+        throw "invalid_action";
+      }
+    } catch (error) {
+      // @ts-ignore
+      return await interaction.reply({
+        content: iTranslate(guild, `${REJECTION_PATH}.${error}`),
+      });
+    }
+  },
 };
--- subcommands/unequip.js
+++ subcommands/unequip.js
@@ -1,49 +1,68 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'),
-	{ getItemList } = require('../handlers/itemInventory'),
-	{ iTranslate } = require('../handlers/language');
+const { SlashCommandBuilder } = require("@discordjs/builders"),
+  { getItemList } = require("../handlers/itemInventory"),
+  { iTranslate } = require("../handlers/language");
 
-const TRANSLATION_PATH = 'subcommands.unequip';
-const REJECTION_PATH = 'rejection.items';
+const TRANSLATION_PATH = "subcommands.unequip";
+const REJECTION_PATH = "rejection.items";
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('unequip')
-		.setDescription('Unequip selected item.'),
-	/**
-		 *
-		 * @param {import('discord.js').Interaction} interaction
-		 * @param {import('../models/profileSchema').User} profileData
-		 * @returns
-		 */
-	async execute(interaction, profileData) {
-		/** @type {String} */
-		// @ts-ignore
-		const itemAction = interaction.options.getString('item'),
-			guild = interaction.guild;
-		const itemList = getItemList();
-		try {
-			const currentItem =
-                    itemList[Object.keys(itemList).filter((item) => itemList[item].name.toLowerCase() === itemAction.toLowerCase())
-                    ];
-			if (!currentItem) throw 'item_not_found';
-			else if (!profileData.equipment.includes(currentItem.id)) throw 'item_not_equipped';
-			// Remove item from equipment
-			profileData.equipment = profileData.equipment.filter((item) => item !== currentItem.id);
-			// Add item to inventory
-			// @ts-ignore
-			profileData.inventory.push({
-				_id: currentItem.id,
-				quantity: 1,
-			});
-			// @ts-ignore
-			await profileData.save();
-			// @ts-ignore
-			await interaction.reply({ content: iTranslate(guild, `${TRANSLATION_PATH}.success`, { currentItem }) });
-		}
-		catch (err) {
-			// @ts-ignore
-			if (typeof err !== 'string') return interaction.reply(iTranslate(guild, 'error', { ns: 'common' }));
-			// @ts-ignore
-			interaction.reply(iTranslate(guild, `${REJECTION_PATH}.${err}`, { ns: 'glossary' }));
-		}
-	},
+  data: new SlashCommandBuilder()
+    .setName("unequip")
+    .setDescription("Unequip selected item."),
+  /**
+   *
+   * @param {import('discord.js').BaseInteraction} interaction
+   * @param {import('../models/profileSchema').User} profileData
+   * @returns
+   */
+  async execute(interaction, profileData) {
+    /** @type {String} */
+    // @ts-ignore
+    const itemAction = interaction.options.getString("item"),
+      guild = interaction.guild;
+    const itemList = getItemList();
+    try {
+      const currentItem =
+        itemList[
+          Object.keys(itemList).filter(
+            (item) =>
+              itemList[item].name.toLowerCase() === itemAction.toLowerCase()
+          )
+        ];
+      if (!currentItem) {
+        throw "item_not_found";
+      } else if (!profileData.equipment.includes(currentItem.id)) {
+        throw "item_not_equipped";
+      }
+      // Remove item from equipment
+      profileData.equipment = profileData.equipment.filter(
+        (item) => item !== currentItem.id
+      );
+      // Add item to inventory
+      // @ts-ignore
+      profileData.inventory.push({
+        _id: currentItem.id,
+        quantity: 1,
+      });
+      // @ts-ignore
+      await profileData.save();
+      // @ts-ignore
+      await interaction.reply({
+        content: iTranslate(guild, `${TRANSLATION_PATH}.success`, {
+          currentItem,
+        }),
+      });
+    } catch (err) {
+      // @ts-ignore
+      if (typeof err !== "string") {
+        // @ts-ignore
+        return interaction.reply(
+          { content: iTranslate(guild, "error", { ns: "common", user: interaction.user }) }
+        );
+      }
+      // @ts-ignore
+      interaction.reply(
+        iTranslate(guild, `${REJECTION_PATH}.${err}`, { ns: "glossary" })
+      );
+    }
+  },
 };
