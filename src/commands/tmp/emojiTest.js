1ad4c685b78d2592c5d2984a0c63ab57fa30476c
Acr0l <sb.lorca.g@gmail.com>
2022-11-20 17:44:31 -0300

    test v14

--- src/commands/tmp/emojiTest.js
+++ src/commands/tmp/emojiTest.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,12 +8,10 @@ module.exports = {
 		.setDescription('Just testing if the emoji is working'),
 	/**
     * @param { Object } interaction - Object with the interaction data sent by the user.
-    * @param { Object } profileData - Object from the database with the user profile data.
-    * @param { Object } client - The discord.js client object.
     */
 	async execute(interaction) {
 		const emoji = '<:HintUsed:905990062095867935>';
-		const embed = new MessageEmbed()
+		const embed = new EmbedBuilder()
 			.setTitle('Emoji Test')
 			.setDescription('This is a test of the emoji ' + emoji)
 			.setColor('#00ff00')
